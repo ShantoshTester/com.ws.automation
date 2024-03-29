ce9c63f553e44f50eaf28f56e8d4f21b38ef382f
shantosh <shantosh25@hotmail.com>
2024-03-29 07:33:10 +0530

    added password recovery page

--- src/main/java/com/ws/pages/PasswordRecoverPage.java
+++ src/main/java/com/ws/pages/PasswordRecoverPage.java
@@ -1,7 +1,59 @@
 package com.ws.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 import com.ws.base.DriverScript;
 
 public class PasswordRecoverPage extends DriverScript {
+
+//***************************************** Page Elements ***************************************** //
+	
+	@FindBy(xpath = "//h1[text()='Password recovery']") WebElement passwordRecoveryText;
+	@FindBy(xpath = "//input[@id='Email']") WebElement passwordrecoveryEmailTextbox;
+	@FindBy(name = "send-email") WebElement recoverButton;
+	@FindBy(xpath = "//span[@for='Email']") WebElement wrongEmailMsg;
+	@FindBy(xpath = "//div[@class='result']") WebElement emailInstructionsSentMsg;
+
+//***************************************** Page Initialization *********************************** //
+	
+	public PasswordRecoverPage()
+	{
+		PageFactory.initElements(driver, this);
+	}
+	
+//***************************************** Page Actions/Methods ************************************ //
+	
+	public String getWrongEmailMsg()
+	{
+		return wrongEmailMsg.getText();
+	}
+	
+	public String getEmailSentMsg()
+	{
+		return emailInstructionsSentMsg.getText();
+	}
+	
+	public void clickRecoverButton()
+	{
+		recoverButton.click();
+	}
+	
+	public void enterUserEmail(String email)
+	{
+		passwordrecoveryEmailTextbox.clear();
+		passwordrecoveryEmailTextbox.sendKeys(email);
+	}
+	
+	public boolean isPasswordRecoveryDisplayed()
+	{
+		return passwordRecoveryText.isDisplayed();
+	}
+	
+	public String getPasswordRecoveryText()
+	{
+		return passwordRecoveryText.getText();
+	}
 
 }
